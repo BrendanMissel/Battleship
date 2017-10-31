9b383931f93fa7ef42f5fc94a0e511347ebd9630
Brendan <bmissel68@gmail.com>
2017-10-31 15:31:44 -0500

    Passing test for testOutOfBounds: attempting to place a ship outside the board

--- Model.py
+++ Model.py
@@ -5,7 +5,7 @@ class Model:
     @staticmethod
     def place_boat(board, ship):
         try:
-            if Model.validate_loc(board, ship):
+            if Model.validate_loc(ship):
                 if ship.orientation == 'v':
                     for i in range(0, ship.size):
                         board.board[ship.head[0]+i][ship.head[1]] = 'O'
@@ -31,5 +31,14 @@ class Model:
             print('Shot is invalid. Try again.')
 
     @staticmethod
-    def validate_loc(board, ship):
-        return True
+    def validate_loc(ship):
+        if ship.orientation == 'v':
+            if ship.head[0] + ship.size > 9:
+                return False
+            else:
+                return True
+        elif ship.orientation == 'h':
+            if ship.head[1] + ship.size > 9:
+                return False
+            else:
+                return True
