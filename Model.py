6039690219052e86bfba6b1812b5a38b8ef68356
Brendan <bmissel68@gmail.com>
2017-10-31 15:00:11 -0500

    Passing test for testFireShot: placing a ship on the board, and registering a hit or a miss

--- Model.py
+++ Model.py
@@ -1,4 +1,4 @@
-# model class
+# Model class
 
 
 class Model:
@@ -8,6 +8,7 @@ class Model:
             if ship.orientation == 'v':
                 for i in range(0, ship.size):
                     board.board[ship.head[0]+i][ship.head[1]] = 'O'
+
             elif ship.orientation == 'h':
                 for i in range(0, ship.size):
                     board.board[ship.head[0]][ship.head[1]+i] = 'O'
@@ -16,4 +17,14 @@ class Model:
 
     @staticmethod
     def fire_shot(board, loc):
-        pass
+        try:
+            if board.board[loc[0]][loc[1]] == 'O':
+                board.board[loc[0]][loc[1]] = 'X'  # X represents a hit on a ship.
+                board.score += 100
+
+            elif board.board[loc[0]][loc[1]] == ' ':
+                board.board[loc[0]][loc[1]] = '*'  # * represents a miss on the board.
+                board.score -= 10
+
+        except:
+            print('Shot is invalid. Try again.')
