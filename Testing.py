77d838e375c7f057cf018c7169f040c82f116e20
Brendan <bmissel68@gmail.com>
2017-10-31 11:27:24 -0500

    Failing test for testBoardInit: checking the size of the board and that the board is completely empty.

--- Testing.py
+++ Testing.py
@@ -8,7 +8,20 @@ from Ship import Ship
 
 
 class BattleshipTest(unittest.TestCase):
-    pass
+    def testBoardInit(self):
+        test_object = Board()
+
+        # test the size of the Board object
+        expected_value = 10
+        actual_value = 0
+
+        assert(actual_value == expected_value)
+
+        # test that the board is empty
+        expected_value = ' '
+        actual_value = ''
+        assert(actual_value == expected_value)
 
 if __name__ == '__main__':
-    pass
+    b = BattleshipTest()
+    b.testBoardInit()
